a5f7febae969f3132db9888289b29bacca36ca5a
Rafael Renigado <renigado@aol.com>
2023-03-01 15:44:23 +0000

    done

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,4 @@
-
+CREATE TABLE song (title VARCHAR(20), artist VARCHAR(100));
+INSERT INTO song VALUES('Let it be', 'Beatles');
+INSERT INTO song VALUES('Hotel California', 'Eagles');
+INSERT INTO song VALUES('Kashmir', 'Led Zeppelin');
